ec833dd379eaf20f45032f1afa6a6f3d3cb3f289
Leon Nothegger <nothegger.leon@gmail.com>
2021-08-23 00:39:31 +0200

    added notes

---
+++ src/components/results.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <!--  API key: 53459456819c4042b7cae0f9610b0a51
+      Documentation: https://newsapi.org/docs -->
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
