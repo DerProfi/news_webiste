4e127a61c69ab37ae3874efec5138c27279923f0
Leon Nothegger <nothegger.leon@gmail.com>
2021-08-23 16:30:58 +0200

    added fetch

--- src/components/results.vue
+++ src/components/results.vue
@@ -3,13 +3,28 @@
   <p>This page will show the results</p>
     <!--  API key: 53459456819c4042b7cae0f9610b0a51
       Documentation: https://newsapi.org/docs -->
+      <button @click="getNews">Click</button>
 </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    getNews: function getNews(e) {
+    e.preventDefault()
 
+    const apiKey = '53459456819c4042b7cae0f9610b0a51'
+    let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${apiKey}`
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+    }
+  }
 }
+
+
+
 </script>
 
 <style>
